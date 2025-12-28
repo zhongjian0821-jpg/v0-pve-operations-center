b109bf25629a47ad1e45d3bf0b6a82d5ecb90f8f
zhongjian0821-jpg <zhongjian0821@gmail.com>
2025-12-28 17:07:44 +0800

    feat: add password utility library

---
+++ lib/password.ts
@@ -0,0 +1,13 @@
+import bcrypt from 'bcryptjs';
+
+export async function hashPassword(password: string): Promise<string> {
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
+}
+
+export async function comparePassword(
+  password: string,
+  hash: string
+): Promise<boolean> {
+  return bcrypt.compare(password, hash);
+}
